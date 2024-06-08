15ff3cc5688f14a3ec808cc227f701a990db466b
Adong <51040698@qq.com>
2024-06-08 10:22:47 +0800

    v0.5

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 git add .
 read -t 30 -p '请输入Tag信息:' tag_name
-git commit -m "$tag_name"
-git tag tag_name
+git commit -m $tag_name
+git tag $tag_name
 
-git remote add origin git@github.com:iStudyMen/lume-theme-blog.git || git push -u origin tag_name
+git remote add origin git@github.com:iStudyMen/lume-theme-blog.git || git push -u origin $tag_name
