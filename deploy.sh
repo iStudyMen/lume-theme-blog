967b72abd5208391975677e5635e2fe1b7ba1fbf
Adong <51040698@qq.com>
2024-06-08 10:20:52 +0800

    v0.4

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@
 # 确保脚本抛出遇到的错误
 set -e
 git add .
-read -t 30 -p '请输入commit信息:' commit_log 
-git commit -m "$commit_log"
+read -t 30 -p '请输入Tag信息:' tag_name
+git commit -m "$tag_name"
+git tag tag_name
 
-git remote add origin git@github.com:iStudyMen/lume-theme-blog.git || git push -u origin main
+git remote add origin git@github.com:iStudyMen/lume-theme-blog.git || git push -u origin tag_name
