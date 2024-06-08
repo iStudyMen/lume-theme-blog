5db3c2401c9027eb0f3e413e24f490c5dfb2d9c1
Adong <51040698@qq.com>
2024-06-08 10:50:25 +0800

    patch post.vto

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,9 @@
 # 确保脚本抛出遇到的错误
 set -e
 git add .
+read -t 30 -p '请输入Commit信息:' commit_info
+git commit -m "$commit_info"
 read -t 30 -p '请输入Tag信息:' tag_name
-git commit -m $tag_name
 git tag $tag_name
 
 git remote add origin git@github.com:iStudyMen/lume-theme-blog.git || git push -u origin $tag_name
